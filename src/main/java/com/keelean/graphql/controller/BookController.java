062e9e2a0d2a5ecd70304a04686cdc048b8dd0d1
Kingsley Amaeze <cmkslee@gmail.com>
2025-02-08 19:28:33 +0100

    First commit

---
+++ src/main/java/com/keelean/graphql/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.keelean.graphql.controller;
+
+import com.keelean.graphql.model.Author;
+import com.keelean.graphql.model.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
